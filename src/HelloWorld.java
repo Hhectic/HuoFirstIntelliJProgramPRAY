5aa90092c05626d543b0a18221c044f0485fc3b3
AngelChen914 <angelc470@nycstudents.net>
2022-09-28 12:14:23 -0400

    added print statement

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String [] args)
+    {
+        System.out.print("Hello World!");
+        System.out.print("whoever uses this computer before period 6, YOUR A BOZO");
+    }
 }
